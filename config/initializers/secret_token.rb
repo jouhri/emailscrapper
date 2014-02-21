98c11c730a73a390d169b20b2dfdb31eb50eb0a5
Oualid <jouhri.oualid@gmail.com>
2014-02-21 16:38:30 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emailscrapper::Application.config.secret_key_base = '7c98470f92ae44f1e57e54e3b8e846fececd6a69eb4814f525c11891a7d3111d982d3e74c413a50e36ff9e9c8e5385bca8bb73d726c9cfb3cb5bcb2fe220ec92'
