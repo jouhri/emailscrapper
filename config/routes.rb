32af723326f60a264ef6f1032c8bd5275126ad61
Oualid <jouhri.oualid@gmail.com>
2014-02-21 17:04:24 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Emailscrapper::Application.routes.draw do
 
   root 'orders#new'
-  resources :orders
+  resources :orders, only: [:show, :new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
